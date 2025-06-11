1bdede20efd17ed1dc264fed72f4bc88325575e0
Muratsuleymanoglu <22010310067@ogrenci.bartin.edu.tr>
2025-06-12 02:30:57 +0300

    Proje Dosyaları Yüklendi

---
+++ FrmRaporlar.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace personel_kayit_programi
+{
+    partial class FrmRaporlar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.tblPersonelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personelVeriTabaniDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personelVeriTabaniDataSet = new personel_kayit_programi.PersonelVeriTabaniDataSet();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.tbl_PersonelTableAdapter = new personel_kayit_programi.PersonelVeriTabaniDataSetTableAdapters.Tbl_PersonelTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.tblPersonelBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personelVeriTabaniDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personelVeriTabaniDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tblPersonelBindingSource
+            // 
+            this.tblPersonelBindingSource.DataMember = "Tbl_Personel";
+            this.tblPersonelBindingSource.DataSource = this.personelVeriTabaniDataSetBindingSource;
+            // 
+            // personelVeriTabaniDataSetBindingSource
+            // 
+            this.personelVeriTabaniDataSetBindingSource.DataSource = this.personelVeriTabaniDataSet;
+            this.personelVeriTabaniDataSetBindingSource.Position = 0;
+            // 
+            // personelVeriTabaniDataSet
+            // 
+            this.personelVeriTabaniDataSet.DataSetName = "PersonelVeriTabaniDataSet";
+            this.personelVeriTabaniDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.tblPersonelBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "personel_kayit_programi.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // tbl_PersonelTableAdapter
+            // 
+            this.tbl_PersonelTableAdapter.ClearBeforeFill = true;
+            // 
+            // FrmRaporlar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "FrmRaporlar";
+            this.Text = "FrmRaporlar";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FrmRaporlar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tblPersonelBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personelVeriTabaniDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personelVeriTabaniDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource personelVeriTabaniDataSetBindingSource;
+        private PersonelVeriTabaniDataSet personelVeriTabaniDataSet;
+        private System.Windows.Forms.BindingSource tblPersonelBindingSource;
+        private PersonelVeriTabaniDataSetTableAdapters.Tbl_PersonelTableAdapter tbl_PersonelTableAdapter;
+    }
+}
---
+++ Frmgrafik.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace personel_kayit_programi
+{
+    public partial class Frmgrafik : Form
+    {
+        public Frmgrafik()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=RASIT\\SQLEXPRESS;Initial Catalog=PersonelVeriTabani;Integrated Security=True;Encrypt=False;TrustServerCertificate=True");
+
+        private void Frmgrafik_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                SqlCommand komutg1 = new SqlCommand("sp_GrafikSehirPersonelSayisi", baglanti);
+                komutg1.CommandType = CommandType.StoredProcedure; 
+                SqlDataReader dr1 = komutg1.ExecuteReader();
+                while (dr1.Read())
+                {
+                    chart1.Series["sehirler"].Points.AddXY(dr1[0], dr1[1]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Şehir grafiği yüklenirken hata oluştu: " + ex.Message);
+            }
+            finally
+            {
+                if (baglanti.State == ConnectionState.Open)
+                    baglanti.Close();
+            }
+
+            try
+            {
+                baglanti.Open();
+                SqlCommand komutg2 = new SqlCommand("sp_GrafikMeslekOrtalamaMaas", baglanti);
+                komutg2.CommandType = CommandType.StoredProcedure; 
+                SqlDataReader dr2 = komutg2.ExecuteReader();
+                while (dr2.Read())
+                {
+                    chart2.Series["meslek-maas"].Points.AddXY(dr2[0], dr2[1]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Meslek-Maaş grafiği yüklenirken hata oluştu: " + ex.Message);
+            }
+            finally
+            {
+                if (baglanti.State == ConnectionState.Open)
+                    baglanti.Close();
+            }
+        }
+    }
+}
